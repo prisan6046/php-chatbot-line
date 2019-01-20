5153c09be7837f655fa4d12f026442a9da3f8a64
prisan6046 <prisan_big_bboy@hotmail.com>
2019-01-21 02:31:04 +0700

    first commit

---
+++ webhooks.php
@@ -0,0 +1,53 @@
+<?php 
+
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
+$access_token = "v/afe7XjTUu/sLsIibylCT/DjF89Rq6gYXsGFw0y1Q1wey/mECGMwaesTsbdj2NZQOpYICaGibHx2nQ0DmGkooCyyU1ezLHy5b671IfIri9St2l9NI/T3z7cOA589qkEDaloctjhl0iT8CaDGxVG6QdB04t89/1O/w1cDnyilFU=";
+
+
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
+error_log($events['events']);
+
+if (!is_null($events['events'])) {
+	foreach ($events['events'] as $event) {
+	
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			
+			error_log($event['message']['text']);
+			$text = $event['message']['text'];
+			$replyToken = $event['replyToken'];
+			$messages = setText($text);
+			sentToLine( $replyToken , $access_token  , $messages );
+		}
+	}
+}
+
+function setText( $text){
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
+	return $messages;
+}
+
+function sentToLine($replyToken , $access_token  , $messages ){
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+		'replyToken' => $replyToken,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+}
